602f8fb35a26fbf2f5af9be2ccf6a3adcddaab2b
Nadun <110454669+naduncalcey@users.noreply.github.com>
2023-08-26 23:32:25 +0530

    [bug] - fixed static height issue in safari ios mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-[100dvh] mx-auto">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
