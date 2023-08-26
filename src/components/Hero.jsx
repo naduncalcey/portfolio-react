3da559f45b760758403076b56a49a2329d655961
Nadun <110454669+naduncalcey@users.noreply.github.com>
2023-08-26 21:33:00 +0530

    [feature] - mobile responsive styles for hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,11 +18,32 @@ const Hero = () => {
             <span className="text-[#915eff]">&nbsp;Nadun</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visual graphics,<br className="sm:block hidden" /> user interfaces and web applications.
+            I develop 3D visual graphics,
+            <br className="sm:block hidden" /> user interfaces and web
+            applications.
           </p>
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev 
+              animate={
+                {
+                  y:[0, 24, 0]
+                }
+              }
+              transition={{
+                duration: 2.0,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,13 +3,13 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./workbench/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={2} groundColor="black" />
       <pointLight intensity={10} />
-      <spotLight 
+      <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
@@ -17,10 +17,10 @@ const Computers = () => {
         castShadow
         shadow-mapSize={1024}
       />
-      <primitive 
+      <primitive
         object={computer.scene}
-        scale={2}
-        position={[0, -3, 0]}
+        scale={ isMobile ? 1.4 :1.9 }
+        position={isMobile ? [0, -2, 0]: [0, -3, 0]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -28,6 +28,29 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
@@ -41,7 +64,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
